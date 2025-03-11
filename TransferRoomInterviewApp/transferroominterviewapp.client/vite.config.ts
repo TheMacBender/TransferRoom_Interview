854906576f3f3f21ad313cb442f37d22d9ad585a
janbe <jan.belczacki.it@gmail.com>
2025-03-11 19:41:00 +0100

    Added staticwebapp.config.json to control client-side routing

--- TransferRoomInterviewApp/transferroominterviewapp.client/vite.config.ts
+++ TransferRoomInterviewApp/transferroominterviewapp.client/vite.config.ts
@@ -1,11 +1,20 @@
 import { fileURLToPath, URL } from 'node:url';
+import path from 'node:path';
 
-import { defineConfig } from 'vite';
+import { defineConfig, normalizePath } from 'vite';
 import plugin from '@vitejs/plugin-react';
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin()],
+    plugins: [plugin(), viteStaticCopy({
+        targets: [
+            {
+                src: normalizePath(path.resolve(__dirname, 'staticwebapp.config.json')),
+                dest: './'
+            }
+        ]
+    })],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
