ed3582f97556be49ea77bc8bf1abc5c24c53b030
janbe <jan.belczacki.it@gmail.com>
2025-03-11 20:04:00 +0100

    Enable Swagger on production for now so that I have a route to test published API

--- TransferRoomInterviewApp/TransferRoomInterviewApp.Server/Program.cs
+++ TransferRoomInterviewApp/TransferRoomInterviewApp.Server/Program.cs
@@ -34,11 +34,11 @@ namespace TransferRoomInterviewApp.Server
             app.UseStaticFiles();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            //if (app.Environment.IsDevelopment())
+            //{
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            //}
 
             app.UseHttpsRedirection();
 
