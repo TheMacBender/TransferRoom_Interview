0efc1c4161dd1741c7ba5f08cd9e173f565cb7f3
janbe <jan.belczacki.it@gmail.com>
2025-03-12 19:46:26 +0100

    Frontend fix for trying to manually enter wrong team id in url

--- TransferRoomInterviewApp/transferroominterviewapp.client/src/components/team/Team.tsx
+++ TransferRoomInterviewApp/transferroominterviewapp.client/src/components/team/Team.tsx
@@ -1,5 +1,5 @@
 import { Container } from "react-bootstrap";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { useQuery } from "@tanstack/react-query";
 
 import TeamHeader from "./header/TeamHeader";
@@ -7,8 +7,14 @@ import PlayersList from "./players-list/PlayersList";
 import useTeams from "../../hooks/useTeams";
 
 const Team = () => {
+    const navigate = useNavigate();
     const params = useParams();
     const teamId = parseInt(params.id ?? "0");
+
+    if (isNaN(teamId)) {
+        navigate("/404");
+    }
+
     const { getTeamById } = useTeams();
     const { isPending, isError, data, error } = useQuery({
         queryKey: ['getTeamData'],
